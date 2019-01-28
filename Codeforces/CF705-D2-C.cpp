22d583fbc692add1af4c61c37a736602cadc0694
AhmedElsisy <ahmedelsisy44@gmail.com>
2019-01-28 04:20:55 +0200

    updated 705C

--- Codeforces/CF705-D2-C.cpp
+++ Codeforces/CF705-D2-C.cpp
@@ -1,14 +1,11 @@
 /*
-    ordered set is a c++ stl : policy based data structure
-    let s be ordered set stores the unread messages
+    let s be set stores the unread messages
     and adj[x] be the messages of application x
     simply when query of type 2 is happened , iterate over unread messages x
     and query of type 3 , iterate over all messages it's index less than t
 */
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
 #define pb push_back
 #define all(v) (v).begin() , (v).end()
@@ -16,23 +13,19 @@
 #define inf 0x3f3f3f3f
 #define watch(x) cout << (#x) << " is " << (x) << endl
 
-using namespace __gnu_pbds;
 using namespace std;
 typedef long long  ll;
 
-template<typename T>
-using ordered_set =
-tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
 const int N = 3e5 + 4;
 int n , q;
-ordered_set<pair<int , int> > s;
+set<pair<int , int> > s;
 set<int> adj[N];
 
 int main(){
 #ifndef ONLINE_JUDGE
     freopen("input.txt" , "r" , stdin);
 #endif
+    ios::sync_with_stdio(0) , cin.tie(0) , cout.tie(0);
     cin >> n >> q;
 
     int cnt = 0 , message = 1;
